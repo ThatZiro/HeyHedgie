a7d7426d4b853796320f724b2ae7f8c0e4c72332
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-11-22 12:19:52 -0500

    Added Footer

--- website/src/componants/App/App.jsx
+++ website/src/componants/App/App.jsx
@@ -2,6 +2,8 @@ import Header from '../Header/Header.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 import Dashboard from '../Dashboard/Dashboard.jsx';
 import DailyReflection from '../DailyReflection/DailyReflection.jsx';
+import Information from '../Information/Information.jsx';
+import Footer from '../Footer/Footer.jsx'
 function App() {
   return (
     <>
@@ -9,6 +11,8 @@ function App() {
       <Navigation/>
       <Dashboard/>
       <DailyReflection/>
+      <Information/>
+      <Footer/>
     </>
   )
 }
---
+++ website/src/componants/Information/Information.jsx
@@ -0,0 +1,46 @@
+import './Information.css'
+function Information() {
+  return (
+    <>
+      <div className="information">
+        <div className="card">
+          <div className="info-header">
+            <img className="icon" src="/buddies/25282b20c68038aee45cea468c322820.png" alt=""/>
+            <h1>Meet Hedgie</h1>
+          </div>
+          <p>Get to know, Hedgie, on a personal level!  Dive into her profile to uncover charming stories and delightful adventures, and forge a special connection with your newfound buddy.</p>
+          <button>MEET</button>
+        </div>
+        <div className="card">
+          <div className="info-header">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+              <g clipPath="url(#clip0_37_198)">
+                <path d="M34.7222 0H15.2778C12.9774 0 11.1024 2.12891 11.1892 4.70703C11.2066 5.22461 11.224 5.74219 11.25 6.25H2.08333C0.928819 6.25 0 7.29492 0 8.59375C0 17.6367 2.90799 23.9258 6.81424 28.1934C10.6597 32.4023 15.3472 34.5215 18.8021 35.5957C20.8333 36.2305 22.2222 38.1348 22.2222 40.0488C22.2222 42.0898 20.7465 43.75 18.9323 43.75H16.6667C15.1302 43.75 13.8889 45.1465 13.8889 46.875C13.8889 48.6035 15.1302 50 16.6667 50H33.3333C34.8698 50 36.1111 48.6035 36.1111 46.875C36.1111 45.1465 34.8698 43.75 33.3333 43.75H31.0677C29.2535 43.75 27.7778 42.0898 27.7778 40.0488C27.7778 38.1348 29.158 36.2207 31.1979 35.5957C34.6615 34.5215 39.349 32.4023 43.1944 28.1934C47.092 23.9258 50 17.6367 50 8.59375C50 7.29492 49.0712 6.25 47.9167 6.25H38.75C38.776 5.74219 38.7934 5.23438 38.8108 4.70703C38.8976 2.12891 37.0226 0 34.7222 0ZM4.24479 10.9375H11.5712C12.3611 19.7363 14.1059 25.6152 16.0764 29.5508C13.9149 28.4766 11.6667 26.9629 9.72222 24.834C6.94444 21.7969 4.6875 17.4121 4.25347 10.9375H4.24479ZM40.2865 24.834C38.342 26.9629 36.0937 28.4766 33.9323 29.5508C35.9028 25.6152 37.6476 19.7363 38.4375 10.9375H45.7639C45.3212 17.4121 43.0642 21.7969 40.2951 24.834H40.2865Z" fill="#F5186F"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_37_198">
+                  <rect width="50" height="50" fill="white"/>
+                </clipPath>
+              </defs>
+            </svg>
+            <h1>Sticker Contest</h1>
+          </div>
+          <p>Unleash your creativity in our Sticker Contest! Join the fun, design unique icons, and stand a chance to win exciting prizes while spreading joy through the power of imaginative expression.</p>
+          <button>MORE INFO</button>
+        </div>
+        <div className="card">
+          <div className="info-header">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+              <path d="M43.0572 24.2867V24.6206L48.6759 17.568C50.4414 15.3521 50.4414 11.7702 48.6759 9.55429L42.3801 1.66194C40.6147 -0.553982 37.761 -0.553982 35.9956 1.66194L32.763 5.71942C32.5453 5.68906 32.3196 5.66883 32.0939 5.66883H23.0653C20.0746 5.66883 17.6159 8.50197 17.2934 12.1446H17.2612V24.2867C17.2612 26.5228 18.7042 28.334 20.4857 28.334C22.2672 28.334 23.7102 26.5228 23.7102 24.2867V17.0014V16.9913V15.3825H25H35.9633C35.9633 15.3825 35.9633 15.3825 35.9714 15.3825H36.6082C40.1713 15.3825 43.0572 19.0049 43.0572 23.4772V24.2867ZM26.2898 18.6204V24.2867C26.2898 28.3138 23.6941 31.5719 20.4857 31.5719C17.2773 31.5719 14.6816 28.3138 14.6816 24.2867V12.2862C11.7876 12.9136 9.37727 15.5545 8.55502 19.187L7.22491 25.0152L1.32406 32.4218C-0.441354 34.6378 -0.441354 38.2197 1.32406 40.4356L7.61991 48.3381C9.38533 50.554 12.239 50.554 14.0044 48.3381L17.0435 44.5234C17.1161 44.5234 17.1886 44.5335 17.2612 44.5335H30.1592C32.2954 44.5335 34.0286 42.3581 34.0286 39.6767C34.0286 39.1101 33.948 38.5637 33.811 38.0578H34.0286C36.1649 38.0578 37.898 35.8823 37.898 33.201C37.898 31.9058 37.495 30.7321 36.8339 29.8619C38.9057 29.356 40.4696 27.0692 40.4776 24.317V24.2765C40.4696 21.1601 38.4543 18.6305 35.9633 18.6305H26.2898V18.6204Z" fill="#FFDA1B"/>
+            </svg>
+            <h1>Support Us</h1>
+          </div>
+          <p>Your generosity powers our journey of self-discovery and community building. Help us grow and make a positive impact by donating today.</p>
+          <button>SUPPORT</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Information
